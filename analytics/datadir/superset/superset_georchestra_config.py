2beb066e23d771d8eb75078edb94fa5e1b296e63
Florian Necas <florian.necas@camptocamp.com>
2026-04-20 13:40:04 +0200

    feat: clean up Geo2France color schemes and update vector configuration

--- analytics/datadir/superset/superset_georchestra_config.py
+++ analytics/datadir/superset/superset_georchestra_config.py
@@ -97,73 +97,10 @@ HOME_PAGE_VIEW = "DashboardModelView.list"
 
 DOCUMENTATION_URL = "https://docs.georchestra.org/en/superset/"
 
-################################
-# Geo2france custom config
-################################
 
-EXTRA_CATEGORICAL_COLOR_SCHEMES = [
-    {
-        "id": 'Geo2FranceDefault',
-        "description": '',
-        "label": 'Geo2France Default',
-        "isDefault": False,
-        "colors":
-            ['#2f85cd', '#7900c4', '#f700a6', '#ff5754', '#ffb255', '#fbf752',
-             '#8eec46', '#b2d9cb']
-    },
-    {
-        "id": 'Geo2FranceAlternative',
-        "description": '',
-        "label": 'Geo2France Alternative',
-        "isDefault": False,
-        "colors":
-            ['#dfe4ea', '#5892ee', '#8bc832', '#8899a8', '#155fd5', '#456218',
-             '#061d41']
-    },
-    {
-        "id": 'Geo2FranceFull',
-        "description": '',
-        "label": 'Geo2FranceFull',
-        "isDefault": False,
-        "colors":
-            ['#2f85cd', '#7900c4', '#f700a6', '#ff5754', '#ffb255', '#fbf752',
-             '#8eec46', '#b2d9cb', '#005fb2', '#2900a7', '#c6007c', '#ff3001',
-             '#ff9000', '#fee300', '#8bc800', '#00b966', '#6ca6d8', '#836ad2',
-             '#e370b9', '#ff9881', '#ffc781', '#fff07e', '#c1e372', '#6ddcaa',
-             '#0f4395', '#210083', '#9f0064', '#cd2601', '#cd7300', '#cdb600',
-             '#709f02', '#007c48']
-    }
-]
+EXTRA_CATEGORICAL_COLOR_SCHEMES = []
 
-EXTRA_SEQUENTIAL_COLOR_SCHEMES = [
-    {
-        "id": 'Geo2FranceDivergenteSmall',
-        "description": '',
-        "isDiverging": True,
-        "label": 'Geo2france Divergente Basic',
-        "isDefault": False,
-        "colors":
-            ['#8BC832', '#3758F9', '#3758F9', '#061D41']
-    },
-    {
-        "id": 'Geo2FranceDivergenteAlternative',
-        "description": '',
-        "isDiverging": True,
-        "label": 'Geo2france Divergente Alternative',
-        "isDefault": False,
-        "colors":
-            ['#dfe4ea', '#0f4395', '#d948a5', '#061d41']
-    },
-    {
-        "id": 'Geo2FranceDivergente',
-        "description": '',
-        "isDiverging": True,
-        "label": 'Geo2france Divergente',
-        "isDefault": False,
-        "colors":
-            ['#DFE4EA', '#8BC832', '#3758F9', '#061D41']
-    },
-]
+EXTRA_SEQUENTIAL_COLOR_SCHEMES = []
 
 ################################
 # Web Security config
@@ -206,12 +143,6 @@ TALISMAN_CONFIG = {
         ],
         # unsafe-eval and cdn are necessary for the geOr header webcomponent to work
         "script-src": ["'self'", "'strict-dynamic'", "'unsafe-eval'", "https://cdn.jsdelivr.net/"],
-
-        # Those 2 lines are related to iframe embedding (e.g. to embed an mviewer map from another domain)
-        # frame-ancestors = allow to embed this superset's visualizations to be embedded in www.geo2france.fr content
-        # frame-src = allow to embed for instance a mviewer map from www.geo2france.fr in this Superset instance (in a dashboard)
-        "frame-ancestors" : "www.geo2france.fr",
-        "frame-src" : ["'self'", "www.geo2france.fr"],
     },
     "content_security_policy_nonce_in": ["script-src"],
     "force_https": False,
